8f15d44e0aa84340fe1e62f61d39b76ad20ab627
Soham Das <soham5959595@gmail.com>
2025-12-27 12:45:54 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Product {
+  image: string;
+  price: number | null;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'product-page';
+  products: Product[] = [
+    { image: 'assets/product1.jpg', price: 1999 },
+    { image: '', price: null },
+    { image: '', price: null }
+  ];
 }
